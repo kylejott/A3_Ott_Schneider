0001fd5d1224aeec1d5dccd14a33bc86ae7e3d08
Kyle Ott <kylejott@gmail.com>
2014-11-07 22:51:59 +0100

    Added first try of loop script
    it’s not working exactly?

---
+++ loop.R
@@ -0,0 +1,46 @@
+#################
+# first scrape
+#################
+
+# Load packages
+library(httr)
+library(dplyr)
+library(XML)
+
+tables = list()
+
+for (i in 1:29){
+
+# URL with the medals table
+URL_temp <- paste0('http://www.taloussanomat.fi/verotiedot/2010/suurituloisimmat/?n=', i)
+
+#### Gather content and parse all tables ####
+table <- URL_temp %>% GET() %>% content(as = 'parsed') %>% readHTMLTable()
+
+# Identify correct table
+names(table) # the table does not have an ID
+# select first table with taxData
+tables[[i]] <- table[[1]]
+##rearrange the list somehow???
+
+}
+
+# Convert to a data frame
+fintax2010 <- as.data.frame(tables)
+
+#### Clean ####
+# Drop unwanted variables
+fintaxes <- select(fintaxes, -NULL.V2, -NULL.V5)
+
+# Give new variable names
+names(fintaxes) <- c('Name', 'Total Rev', 'Total Paid Taxs', 'Tax Rate')
+
+
+
+
+# Convert variable classes
+medals$country <- as.character(medals$country)
+for (i in 2:5) medals[, i] <- as.integer(medals[, i])
+
+# Sort by total medals in descending order
+medals <- arrange(medals, desc(total))
